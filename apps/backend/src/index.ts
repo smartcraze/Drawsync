b93cba8bfed75fe3f6a62e53a02ef5a4ebef5218
smartcraze <smartcraze354@gmail.com>
2025-03-01 02:03:09 +0530

    Structure package name @repo/[name]

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,2 +1,8 @@
 import express from "express";
-import db from "db";
+import db from "@repo/db";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
